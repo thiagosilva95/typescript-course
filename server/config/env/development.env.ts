050fbce92ac5859b2c083e77407172faa42c383d
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-12-24 14:25:03 -0200

    adding config module

---
+++ server/config/env/development.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'development',
+    db: 'ts-api',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'root',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbUrl: 'postgres://postegres:pgroot@localhost:5432/ts-api',
+    secret: 'S3cr3t'
+};
---
+++ server/config/env/test.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'test',
+    db: 'ts-api-test',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'root',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbUrl: 'postgres://postegres:pgroot@localhost:5432/ts-api-test',
+    secret: 'S3cr3t'
+};
