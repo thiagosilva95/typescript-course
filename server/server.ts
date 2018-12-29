8e4fb6a311286f05b6da54e46101939d95c6ce2a
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-12-29 19:15:41 -0200

    refactoring

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ const config = require('./config/env/config');
 
 const server = http.createServer(Api);
 
+
 server.listen(config.serverPort);
 server.on('listening', () => console.log(`Servidor está rodando na porta ${config.serverPort}`));
 server.on('error', (error: NodeJS.ErrnoException) => console.log(`Ocorreu um erro: ${error}`));
