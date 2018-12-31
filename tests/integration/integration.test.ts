a5b4c609700772d18b30384a55620b6f46fc8a71
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-12-31 11:45:15 -0200

    integration tests

---
+++ tests/integration/integration.test.ts
@@ -0,0 +1,33 @@
+import { app, request, expect } from './config/helpers';
+
+describe('Testes de integração', () => {
+    describe('GET /api/users/all', () => {
+        it('Deve retornar um json com todos os usuários', done => {
+
+        });
+    });
+
+    describe('GET /api/users/:id', () => {
+        it('Deve retornar um json com todos os usuários', done => {
+
+        });
+    });
+
+    describe('POST /api/users/new', () => {
+        it('Deve criar um usuário', done => {
+
+        });
+    });
+
+    describe('PUT /api/users/:id/edit', () => {
+        it('Deve atualizar um usuário', done => {
+
+        });
+    });
+
+    describe('DELETE /api/users/:id', () => {
+        it('Deve deletar um usuário', done => {
+
+        });
+    });
+});
---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,27 @@
+import { testDouble, expect } from './config/helpers';
+
+describe('Testes unitários do controller', () => {
+    describe('Método CREATE', () => {
+        it('Deve criar um novo usuário', () => {
+
+        });
+    });
+
+    describe('Método UPDATE', () => {
+        it('Deve atualaizar um usuário', () => {
+
+        });
+    });
+
+    describe('Método GET', () => {
+        it('Deve retornar uma lista com todos os usuários', () => {
+
+        });
+    });
+
+    describe('Método DELETE', () => {
+        it('Deve deletar um usuário', () => {
+
+        });
+    });
+});
