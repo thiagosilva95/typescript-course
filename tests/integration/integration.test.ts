220ea189cc096a9f47b6346b19f1d4c303e71688
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-12-31 12:10:25 -0200

    integration tests

--- tests/integration/integration.test.ts
+++ tests/integration/integration.test.ts
@@ -3,31 +3,59 @@ import { app, request, expect } from './config/helpers';
 describe('Testes de integração', () => {
     describe('GET /api/users/all', () => {
         it('Deve retornar um json com todos os usuários', done => {
-
+            request(app)
+                .get('/api/users/all')
+                .end((error, res) => {
+                    expect(res.status).to.equal(200);
+                });
         });
     });
 
     describe('GET /api/users/:id', () => {
         it('Deve retornar um json com todos os usuários', done => {
-
+            request(app)
+            .get(`/api/users/${1}`)
+            .end((error, res) => {
+                expect(res.status).to.equal(200);
+            });
         });
     });
 
     describe('POST /api/users/new', () => {
         it('Deve criar um usuário', done => {
-
+            const user = {
+                nome: 'Teste'
+            }
+            request(app)
+            .post('/api/users/new')
+            .send(user)
+            .end((error, res) => {
+                expect(res.status).to.equal(200);
+            });
         });
     });
 
     describe('PUT /api/users/:id/edit', () => {
         it('Deve atualizar um usuário', done => {
-
+            const user = {
+                nome: 'TesteUpdate'
+            }
+            request(app)
+            .put(`/api/users/${1}/edit`)
+            .send(user)
+            .end((error, res) => {
+                expect(res.status).to.equal(200);
+            });
         });
     });
 
     describe('DELETE /api/users/:id', () => {
         it('Deve deletar um usuário', done => {
-
+            request(app)
+            .delete(`/api/users/${1}`)
+            .end((error, res) => {
+                expect(res.status).to.equal(200);
+            });
         });
     });
 });
